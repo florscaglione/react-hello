4dfe2096958cfdf47d3eef7222328989d44df005
florscaglione <florencia.scaglione@hotmail.com>
2021-08-16 23:15:39 +0000

    version final CONTADOR

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+//import { ProgressPlugin } from "webpack";
+//include images into your bundle
+//create your first component
+function SecondsCounter(props) {
+	return (
+		<div className="bigCounter">
+			<div className="calendar">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="four">{props.digitFour}</div>
+			<div className="three">{props.digitThree}</div>
+			<div className="two">{props.digitTwo}</div>
+			<div className="one">{props.digitOne}</div>
+		</div>
+	);
+}
+SecondsCounter.propTypes = {
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
+export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,22 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import Home from "./component/home.jsx";
-
+import SecondsCounter from "./component/SecondsCounter.jsx";
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function() {
+	const four = Math.floor(counter / 1000) % 10;
+	const three = Math.floor(counter / 100) % 10;
+	const two = Math.floor(counter / 10) % 10;
+	const one = Math.floor(counter % 10);
+	counter++;
+	ReactDOM.render(
+		<SecondsCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
